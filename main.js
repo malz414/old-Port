6d3e683d12cd38ba01fe947de33bc06caaaf33d4
malz414 <72981910+malz414@users.noreply.github.com>
2022-12-03 05:50:13 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css';
+
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
